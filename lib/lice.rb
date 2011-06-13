a7b7a1e744caa7fe1376ae99cefa05c668da66dd
Enrique García Cota <kikito@gmail.com>
2011-06-14 01:21:48 +0200

    finished configuration

--- lib/lice.rb
+++ lib/lice.rb
@@ -1,5 +1,12 @@
-require "lice/error.rb"
+require "lice/error"
+require "lice/config"
 
 class Lice
+
+  attr_accessor :config
+
+  def initialize(config_path = Lice::Config::DEFAULT_PATH)
+    @config = Lice::Config.new(config_path)
+  end
 
 end
--- test/lice/config_test.rb
+++ test/lice/config_test.rb
@@ -4,14 +4,25 @@ module Test
   module Lice
 
     class Test::Lice::Config < Assertor::Case
-      def initialize()
-        @path = File.join(Dir.pwd, 'config.yml')
-      end
-
-      def test_no_file_should_thow_error
-        assert_raises(::Lice::Error, "config file not found in #{@path}") do
+      def test_no_file_should_raise_error
+        default_path = File.join(Dir.pwd, 'config.yml')
+        assert_raises(::Lice::Error, "No such file or directory - #{default_path}") do
           ::Lice.new()
         end
+      end
+
+      def test_erroneous_file_should_raise_error
+        erroneous_path = File.join(Dir.pwd, 'test', 'files', 'array_config.yml')
+        assert_raises(::Lice::Error, "Invalid config file - #{@path}") do
+          ::Lice.new(erroneous_path)
+        end
+      end
+
+      def test_should_have_default_values
+        empty_path = File.join(Dir.pwd, 'test', 'files', 'empty_config.yml')
+        lice = ::Lice.new(empty_path)
+        assert_equals(Dir.pwd, lice.config.source)
+        assert_equals(File.join(Dir.pwd, '_site'), lice.config.destination)
       end
 
     end
