b2af60211f580fd2cf7eab36bd23a2cdef27d809
Enrique García Cota <kikito@gmail.com>
2011-07-05 00:14:59 +0200

    posts starting to get proper output

--- lib/lice/file.rb
+++ lib/lice/file.rb
@@ -34,18 +34,53 @@ class Lice
         FileUtils.rm_rf(output_path)
         Dir.mkdir(output_path)
       elsif is_post?
-        FileUtils.cp(self.path, output_path)
+        process_post
       else
         FileUtils.cp(self.path, output_path)
       end
     end
 
+    def day
+      name[8..9]
+    end
+    
+    def month
+      name[5..6]
+    end
+
+    def year
+      name[0..3]
+    end
+
+    def slug
+      remove_html(name[11..-1])
+    end
+
     def output_path
-      ::File.join(output_folder, name)
+      if is_post?
+        ::File.join(post_output_folder,  'index.html')
+      else
+        ::File.join(output_folder, name)
+      end
     end
 
     def output_file_exists?
       ::File.exists?(output_path)
+    end
+
+    private
+
+    def remove_html(str)
+      str[/(.+)\.html$/, 1]
+    end
+
+    def post_output_folder
+      ::File.join(output_folder, year, month, day, slug)
+    end
+    
+    def process_post
+      FileUtils.mkdir_p(post_output_folder)
+      FileUtils.cp(self.path, output_path)
     end
 
   end
--- test/lice/file_test.rb
+++ test/lice/file_test.rb
@@ -2,7 +2,7 @@ module Test
   module Lice
     class FileTest < Assertor::Case
       def test_file_starting_with_yyyy_mm_dd_is_a_post
-        f = open_file '2011-01-01-foo.html'
+        f = open_file '2011-01-02-a-title.html'
         assert f.is_post?
       end
 
@@ -14,6 +14,31 @@ module Test
       def test_file_not_ending_in_html_or_polo_is_not_a_post
         f = open_file '2011-01-02-bar.txt'
         assert_not f.is_post?
+      end
+
+      def test_day_of_a_post_returns_the_day
+        f = open_file '2011-01-02-a-title.html'
+        assert_equals '02', f.day
+      end
+
+      def test_month_of_a_post_returns_the_month
+        f = open_file '2011-01-02-a-title.html'
+        assert_equals '01', f.month
+      end
+
+      def test_year_of_a_post_returns_the_year
+        f = open_file '2011-01-02-a-title.html'
+        assert_equals '2011', f.year
+      end
+
+      def test_slug_of_a_post_returns_the_slug
+        f = open_file '2011-01-02-a-title.html'
+        assert_equals 'a-title', f.slug
+      end
+
+      def test_post_files_are_translated_into_a_folder_structure_ending_on_an_index_html
+        f = open_file '2011-01-02-a-title.html'
+        assert_equals f.output_path, ::File.join(output_folder, '2011', '01', '02', 'a-title', 'index.html')
       end
 
       private
